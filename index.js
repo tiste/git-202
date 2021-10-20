747badae0f4872bb6f5251d317b151d431f3db33
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2021-10-20 14:58:41 +0200

    feat: add whisky and sugar

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { Glass } from "./src/glass.js";
 import { shake } from "./src/shake.js";
+import { Whisky } from "./src/whisky.js";
+import { Sugar } from "./src/sugar.js";
 
 async function makeIrish() {
   const recipe = [
     new Glass(),
+    new Whisky(),
+    new Sugar(),
   ];
 
   await shake(recipe);
---
+++ src/whisky.js
@@ -0,0 +1,3 @@
+export class Whisky {
+  volumeAlcohol = 45
+}
