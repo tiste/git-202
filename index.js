2620c2b69b1a1c3c11f936ec2e527aa8ed4a6929
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2021-10-20 15:07:31 +0200

    feat: take coffee beans

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import { Glass } from "./src/glass.js";
 import { shake } from "./src/shake.js";
 import { Whisky } from "./src/whisky.js";
 import { Sugar } from "./src/sugar.js";
+import { Coffee } from "./src/coffee.js";
 
 async function makeIrish() {
   const recipe = [
     new Glass(),
     new Whisky(),
     new Sugar(),
+    new Coffee(),
   ];
 
   await shake(recipe);
